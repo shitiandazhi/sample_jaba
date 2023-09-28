53609b68640e51b69f050a2a88d82f62fcbe7491
shitiandazhi <135299404+shitiandazhi@users.noreply.github.com>
2023-09-28 12:27:42 +0000

    practice3

---
+++ hello3.java
@@ -0,0 +1,16 @@
+public class hello3 {
+  public static void main(String[] args) {
+
+    String str = """
+           おはよう
+           こんにちは
+           こんばんわ
+           """;
+    System.out.println(str);
+  }
+}
+
+
+
+
+
