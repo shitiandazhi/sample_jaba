9c7bb9ca6a7fcc6c9425856c2e68c093dc09e213
shitiandazhi <135299404+shitiandazhi@users.noreply.github.com>
2023-09-29 02:11:18 +0000

    version update

--- hello3.java
+++ hello3.java
@@ -1,16 +1,7 @@
 public class hello3 {
-  public static void main(String[] args) {
-
-    String str = """
-           おはよう
-           こんにちは
-           こんばんわ
-           """;
-    System.out.println(str);
-  }
+    public static void main(String[] args) {
+         String str = "こんにちは\n" +
+                     "こんばんは";
+        System.out.println(str);
+    }
 }
-
-
-
-
-
